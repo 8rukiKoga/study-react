392df3e0ef4ea3fc4a397eaa14e63c6fcfe2bd25
Haruki Koga <harukikoga.dev@gmail.com>
2023-05-30 11:38:13 +0900

    ♻️ Main.jsのimportパスを修正

--- components/Main.js
+++ components/Main.js
@@ -1,9 +1,9 @@
 import styles from './Main.module.css'
 import { Inter } from 'next/font/google'
-import { Links } from "../components/Links"
-import { Logo } from "../components/Logo"
-import { Headline } from "../components/Headline"
-import { Header } from "../components/Header"
+import { Links } from "./Links"
+import { Logo } from "./Logo"
+import { Headline } from "./Headline"
+import { Header } from "./Header"
 
 const inter = Inter({ subsets: ['latin'] })
 
